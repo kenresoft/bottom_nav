6b72fb61b33e7fdb22be1d63d62078be331ed66a
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-04-19 12:38:53 +0100

    Initial commit of this flutter package

---
+++ README.md
@@ -0,0 +1,81 @@
+<!--
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
+
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-library-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/developing-packages).
+-->
+
+A Flutter BottomNavigation package with extra funtions and flexibility and ease of usage.
+
+## Features
+
+- Supports every other widgets aside Icon as [BottomNavigation] Item
+- Supports [BottomNavigation] top [Divider] widget.
+- Support [BottomNavItem] custom background which can either be enable or disabled.
+- [BottomNavigation] Background can be fully customized, images can be applied likewise gradient colors.
+
+### Screenshots
+
+<img src="screenshots/Screenshot_20230419-122006.jpg" width="250" title="BottomNav 1" alt="screenshot_1"/>
+<img src="screenshots/Screenshot_20230419-122758.jpg" width="250" title="BottomNav 2" alt="screenshot_2"/>
+<img src="screenshots/Screenshot_20230419-122847.jpg" width="250" title="BottomNav 3" alt="screenshot_3"/>
+<img src="screenshots/Screenshot_20230419-122954.jpg" width="250" title="BottomNav 4" alt="screenshot_4"/>
+
+## Getting started
+
+To get started with this package just set it up in your [pubspec.yaml] file, thus:
+
+### Dependency
+
+```yaml
+  bottom_nav:
+    git:
+      url: git@github.com:kenresoft/bottom_nav.git
+      ref: master
+```
+
+## Usage
+
+```dart
+ class BottomNavExample extends StatelessWidget {
+  const BottomNavExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNav(
+      labelStyle: const TextStyle(fontStyle: FontStyle.italic),
+      height: 82,
+      padding: const EdgeInsets.all(25).copyWith(top: 5, bottom: 5),
+      backgroundSelected: Colors.blue.shade900,
+      divider: const Divider(height: 0),
+      borderRadius: const BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+      onTap: (index) => buildNavSwitch(index, context, widget.ref),
+      iconSize: 23,
+      labelSize: 20,
+      backgroundColor: Colors.white.withOpacity(0.2),
+      color: Colors.white.withOpacity(0.5),
+      colorSelected: Colors.white,
+      indexSelected: widget.ref.watch(tabProvider.select((value) => value)),
+      items: const [
+        BottomNavItem(label: 'Home', child: CupertinoIcons.home),
+        BottomNavItem(label: 'Book', child: CupertinoIcons.book),
+        BottomNavItem(label: 'Bubble', child: CupertinoIcons.bubble_left),
+        BottomNavItem(label: 'Bookmark', child: CupertinoIcons.bookmark),
+      ],
+    );
+  }
+}
+
+```
+
+## Additional information
+
+This package is not hosted on the pub.dev site yet.
+Feel free to add in your suggestions, contributions and support.
+We keep upgrading this package for better performance even as the dart and flutter frameworks are been updated for the community.
---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({
+    Key? key,
+    required this.items,
+    this.onTap,
+    this.color = Colors.white,
+    this.indexSelected = 0,
+    this.enableItemBackground = false,
+    this.colorSelected = Colors.lightBlueAccent,
+    this.backgroundSelected = Colors.transparent,
+    this.backgroundRadius = const BorderRadius.all(Radius.circular(50)),
+    this.backgroundColor = Colors.grey,
+    this.iconSize = 26,
+    this.labelSize = 11,
+    this.margin,
+    this.padding = const EdgeInsets.all(18.0),
+    this.borderRadius,
+    this.height = 70,
+    this.divider,
+    this.labelStyle = const TextStyle(),
+  }) : super(key: key);
+
+  final List<BottomNavItem> items;
+  final Color color;
+  final Color colorSelected;
+  final bool enableItemBackground;
+  final Color backgroundSelected;
+  final BorderRadiusGeometry backgroundRadius;
+  final Function(int)? onTap;
+  final int indexSelected;
+  final Color backgroundColor;
+  final double iconSize;
+  final double labelSize;
+  final EdgeInsetsGeometry? margin;
+  final EdgeInsetsGeometry padding;
+  final double? height;
+  final BorderRadiusGeometry? borderRadius;
+  final Widget? divider;
+  final TextStyle labelStyle;
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.height,
+      decoration: BoxDecoration(color: widget.backgroundColor, borderRadius: widget.borderRadius),
+      child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        widget.divider == null ? const SizedBox() : widget.divider!,
+        Container(
+          margin: widget.margin,
+          padding: widget.padding,
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            for (var i = 0; i < widget.items.length; ++i)
+              BottomNavItem(
+                label: widget.items[i].label,
+                child: widget.items[i].child is IconData
+                    ? GestureDetector(
+                  onTap: () {
+                    widget.onTap!(i);
+                  },
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    widget.enableItemBackground
+                        ? i == widget.indexSelected
+                        ? Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: widget.backgroundSelected, borderRadius: widget.backgroundRadius),
+                        child: Icon(widget.items[i].child, size: widget.iconSize, color: widget.colorSelected))
+                        : Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: Colors.transparent, borderRadius: widget.backgroundRadius),
+                        child: Icon(widget.items[i].child, size: widget.iconSize, color: widget.color))
+                        : i == widget.indexSelected
+                        ? Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Icon(widget.items[i].child, size: widget.iconSize, color: widget.colorSelected))
+                        : Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Icon(widget.items[i].child, size: widget.iconSize, color: widget.color)),
+                    i == widget.indexSelected
+                        ? Text(widget.items[i].label, style: widget.labelStyle.copyWith(color: widget.colorSelected, fontSize: widget.labelSize))
+                        : Text(widget.items[i].label, style: widget.labelStyle.copyWith(color: widget.color, fontSize: widget.labelSize)),
+                  ]),
+                )
+                    : GestureDetector(
+                  onTap: () => widget.onTap!(i),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    widget.items[i].child,
+                    widget.items[i].child is SizedBox
+                        ? i == widget.indexSelected
+                        ? Text(widget.items[i].label, style: TextStyle(color: widget.colorSelected, fontSize: widget.labelSize))
+                        : Text(widget.items[i].label, style: TextStyle(color: widget.color, fontSize: widget.labelSize))
+                        : const SizedBox(),
+                  ]),
+                ),
+              ),
+          ]),
+        ),
+      ]),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  const BottomNavItem({
+    Key? key,
+    this.child = const SizedBox(),
+    required this.label,
+  }) : super(key: key);
+
+  final dynamic child;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
