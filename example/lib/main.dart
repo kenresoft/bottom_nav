c21a1bd9c485ccfd5e4973e358830da21147159f
kenresoft <amadikenneth8@gmail.com>
2024-02-28 17:01:25 +0100

    Update example project.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,25 +2,64 @@ import 'package:bottom_nav/bottom_nav.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(),
       body: const Center(),
-      bottomNavigationBar: BottomNav(
-          onTap: (p0) {
-            print(p0.toString());
-          },
+      /*bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavItem(
-              label: 'Home',
-              child: ClickScaleIcon(icon: Icons.content_paste_rounded, size: 24, color: Colors.blue),
+            BottomNavigationBarItem(
+              label: '1',
+              icon: Icon(Icons.add_box_rounded),
             ),
-            BottomNavItem(
-              label: 'Home',
-              child: FlutterLogo(),
+            BottomNavigationBarItem(
+              label: '2',
+              icon: Icon(Icons.add_box_rounded),
             ),
-          ]),
-    ),
-  ));
+            BottomNavigationBarItem(
+              label: '3',
+              icon: Icon(Icons.add_box_rounded),
+            ),
+          ],
+        ),*/
+      bottomNavigationBar: BottomNav(
+        onTap: (p0) {
+          print(p0.toString());
+          setState(() {
+            index = p0;
+          });
+        },
+        indexSelected: index,
+        items: const [
+          BottomNavItem(
+            label: 'Home',
+            child: Icon(Icons.content_paste_rounded, size: 24, color: Colors.blue),
+          ),
+          BottomNavItem(
+            label: 'Home',
+            child: FlutterLogo(),
+          ),
+        ],
+      ),
+    );
+  }
 }
